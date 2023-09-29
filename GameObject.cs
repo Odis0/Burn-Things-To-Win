9314c08c93abf31e707a200c77bf869029b922b7
Spencer Osburn <sjosburn@gmail.com>
2023-09-29 16:12:03 -0700

    Revert "I've got it so that there can now be "in-room" descriptions displayed when you move into other rooms. So exciting!"
    This reverts commit 2ba77c00059c8e971700e695de02ae8c39377862.

--- GameObject.cs
+++ GameObject.cs
@@ -38,22 +38,6 @@ public class GameObject : IComponent
         }
         return null;
     }
-
-
-    public T GetObjectComponentOfType<T>(Type componentType) where T : class
-    {
-        foreach (IComponent component in componentList)
-        {
-            if (component.GetType().Equals(componentType))
-            {
-                return (T)component;
-            }
-        }
-        return null;
-    }
-
-
-
     public GameObject(string name = "", params IComponent[] componentList)
     {
         this.name = name;
